a1386ada44d1bd3f9c90fd3e37ffb1882216807f
andrewzhng2 <andrew.zhng2@gmail.com>
2023-01-13 13:16:08 -0500

    Implementing font

--- my-app/App.js
+++ my-app/App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    Sans: require('./assets/fonts/Source_Sans_Pro/SourceSansPro-Bold.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'SourceSansPro-Bold', fontSize: 27 }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
